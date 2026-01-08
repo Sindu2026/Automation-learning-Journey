5f790f3e3860159a3e61bd23d7722fa700097b65
Sindu_87 <sinduja.ranganathan@gmail.com>
2026-01-08 12:30:09 -0600

    Day 1: Java setup and first program execution

---
+++ API Automation Framework/src/test/java/basics/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class FirstJavaProgram { public static void main(String[] args) {
+
+    String testerName = "Sinduja";
+    int experience = 10;
+    boolean learningAutomation = true;
+
+    System.out.println("Tester Name: " + testerName);
+    System.out.println("Experience: " + experience + " years");
+
+    if (learningAutomation) {
+        System.out.println("Transitioning from Manual to Automation 🚀");
+    }
+}
+}
